8bc64ab3c08a45331fd64cda9c0c7a0c8cc4a589
cronbug <dc2580k@163.com>
2023-06-03 02:08:36 +0800

    Update xiaoxiongyouhao.js

--- Loon/xiaoxiongyouhao.js
+++ Loon/xiaoxiongyouhao.js
@@ -4,8 +4,8 @@ let obj = JSON.parse($response.body);
 //console.log(obj)
 
 let vip = {
-  "vip_state": 2,
-	"vip_valid_till_date": "2029\u5e7406\u670809\u65e5",
+    "vip_state": 2,
+    "vip_valid_till_date": "2099\u5e7406\u670809\u65e5",
 }
 
 obj = Object.assign(obj,vip)
