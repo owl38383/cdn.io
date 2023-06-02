b0520ac10f05c38f96750178792f4450b3c65d6a
cronbug <dc2580k@163.com>
2023-06-03 02:01:30 +0800

    Create xiaoxiongyouhao.js

---
+++ Loon/xiaoxiongyouhao.js
@@ -0,0 +1,12 @@
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+//console.log(obj)
+
+let vip = {
+  "vip_state": 2,
+	"vip_valid_till_date": "2029\u5e7406\u670809\u65e5",
+}
+
+obj = Object.assign(obj,vip)
+$done({ body: JSON.stringify(obj)});
