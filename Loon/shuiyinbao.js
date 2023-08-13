906ec22d63216b67685d3ece7809305afe7c764d
cronbug <dc2580k@163.com>
2023-08-13 21:31:36 +0800

    Update shuiyinbao.js

--- Loon/shuiyinbao.js
+++ Loon/shuiyinbao.js
@@ -3,10 +3,22 @@ if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 //console.log(obj)
 
-let vip = {
-    "vip_state": 2,
-    "vip_valid_till_date": "2099\u5e7406\u670809\u65e5",
+let vipInfo = {
+   {
+        "beginTime": "2023-08-13 21:25:24",
+        "createTime": "2023-08-13 21:25:23",
+        "expireTime": "2099-08-21 21:25:24",
+        "firstOpenTime": "2023-08-13 21:25:24",
+        "remainDays": 9999999999,
+        "sign": "4Qf3rB45uGo",
+        "typeName": "永久会员",
+        "userSign": "406_LkgLLsg"
+    }
 }
+obj.content.waterVipInfo = vipInfo;
+obj.content.qrCarVipInfo = vipInfo;
+obj.content.qimingVipInfo = vipInfo;
+obj.content.virtualVipInfo = vipInfo;
+obj.content.docConvertorVipInfo = vipInfo;
 
-obj = Object.assign(obj,vip)
 $done({ body: JSON.stringify(obj)});
