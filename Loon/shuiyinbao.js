9a47795814432e18810fb9a75c0a4e118d9ed003
cronbug <dc2580k@163.com>
2023-08-13 21:40:02 +0800

    Update shuiyinbao.js

--- Loon/shuiyinbao.js
+++ Loon/shuiyinbao.js
@@ -4,16 +4,14 @@ let obj = JSON.parse($response.body);
 console.log(obj)
 
 let vipInfo = {
-   {
-        "beginTime": "2023-08-13 21:25:24",
-        "createTime": "2023-08-13 21:25:23",
-        "expireTime": "2099-08-21 21:25:24",
-        "firstOpenTime": "2023-08-13 21:25:24",
-        "remainDays": 9999999999,
-        "sign": "4Qf3rB45uGo",
-        "typeName": "永久会员",
-        "userSign": "406_LkgLLsg"
-    }
+    "beginTime": "2023-08-13 21:25:24",
+    "createTime": "2023-08-13 21:25:23",
+    "expireTime": "2099-08-21 21:25:24",
+    "firstOpenTime": "2023-08-13 21:25:24",
+    "remainDays": 9999999999,
+    "sign": "4Qf3rB45uGo",
+    "typeName": "永久会员",
+    "userSign": "406_LkgLLsg"
 };
 obj.content.waterVipInfo = vipInfo;
 obj.content.qrCarVipInfo = vipInfo;
